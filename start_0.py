790992eedfbe6c1bf3fc7d95e1b459e9fb0d6fc6
celine delaugere <celine.delaugere@gmail.com>
2018-12-02 19:23:58 +0100

    maj en class

--- start_0.py
+++ start_0.py
@@ -5,46 +5,42 @@ from PIL import Image
 import os
 ssl._create_default_https_context = ssl._create_unverified_context
 
+class jpg_scraper:
 
-html_ref = "https://www.photo.rmn.fr/Package/2C6NU0ZFGW4V"
-file_ref="img"
-root_html = "https://www.photo.rmn.fr"
-if not os.path.isdir(file_ref):
-    os.makedirs(file_ref)
+    def __init__(self, html_ref="https://www.photo.rmn.fr/Package/2C6NU0ZFGW4V",file_ref="img",root_html="https://www.photo.rmn.fr"):
 
-def find_img(a):
-    new_a=[]
-    for a_e in a:
-        for i in range(len(a_e)-4):
-            if a_e[i:i+4]=='.jpg':
-                new_a.append(a_e[1:i+4])
-    return new_a
+        self.html_ref = html_ref
+        self.file_ref=file_ref
+        self.root_html = root_html
+        if not os.path.isdir(file_ref):
+            os.makedirs(file_ref)
+
+    def find_img(self,a):
+        new_a=[]
+        for a_e in a:
+            for i in range(len(a_e)-4):
+                if a_e[i:i+4]=='.jpg':
+                    new_a.append(a_e[1:i+4])
+        return new_a
         
 
 
 
-def dl_jpg(adr_html=html_ref, file_root=file_ref, root_h=root_html):
-    a = str(urllib.request.urlopen(adr_html).read())
-    a0 = a.split(' src=')
-    new_a = find_img(a0)
-    
-    for a in new_a:
-        a_22= a.split("/")
-        filename = a_22[-1]
+    def dl_jpg(self):
+        a = str(urllib.request.urlopen(self.html_ref).read())
+        a0 = a.split(' src=')
+        new_a = self.find_img(a0)
         
-        try:
-            urllib.request.urlretrieve("{}{}".format(root_h,a), os.path.join(file_root,filename))
-        except urllib.error.URLError:
-            print("urlerror")
-            print("{}{}".format(root_h,a))
-    return(new_a)
+        for a in new_a:
+            a_22= a.split("/")
+            filename = a_22[-1]
+            
+            try:
+                urllib.request.urlretrieve("{}{}".format(self.root_html,a), os.path.join(sel.file_ref,filename))
+            except urllib.error.URLError:
+                print("urlerror")
+                print("{}{}".format(self.root_html,a))
+        return(new_a)
 
-new_a=dl_jpg()
-print(new_a)
-
-
-
-#a = str(urllib.request.urlopen(html_ref).read())
-#print(type(a))
-#a0=a.split(" src=")
-#print(a0)
+#a = jpg_scraper()
+#a.dl_jpg()
